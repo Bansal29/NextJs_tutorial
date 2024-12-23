0cf916cb4fc001ede687c8f2b57b1934969a943c
Aryan Bansal <91452049+Bansal29@users.noreply.github.com>
2024-12-24 01:07:51 +0530

    added backend

--- my-next-app/app/api/user/route.ts
+++ my-next-app/app/api/user/route.ts
@@ -1,3 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  // should add zod validation here
+  const user = await client.user.create({
+    data: {
+      username: body.username,
+      password: body.password,
+    },
+  });
+
+  console.log(user.id);
+
+  return NextResponse.json({ message: "Signed up" });
+}
 export async function GET() {
-  return Response.json({ username: "Aryan", email: "aryan@gmail.com" });
+  const user = await client.user.findFirst({});
+  return Response.json({ name: user?.username, email: user?.username });
 }
