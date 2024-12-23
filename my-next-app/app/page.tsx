c302b7e19373e7d98be8270025377fa967a9a86c
Aryan Bansal <91452049+Bansal29@users.noreply.github.com>
2024-12-23 23:35:27 +0530

    backend

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -1,5 +1,22 @@
-import Image from "next/image";
+import axios from "axios";
 
-export default function Home() {
-  return <div>Hi there!</div>;
+async function getUserDetails() {
+  const response = await axios.get("http://localhost:3000/api/user");
+  return response.data;
+}
+
+export default async function Home() {
+  const userData = await getUserDetails();
+
+  return (
+    <div className="flex flex-col justify-center h-screen">
+      <div className="flex justify-center">
+        <div className="border p-8 rounded">
+          <div>Name: {userData?.username}</div>
+
+          {userData?.email}
+        </div>
+      </div>
+    </div>
+  );
 }
